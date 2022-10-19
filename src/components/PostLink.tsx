97e0a0154db65c7da503016b9a6e494aaa0e8889
snassr <snassr.x@gmail.com>
2022-10-18 20:43:00 -0700

    Added components ui

---
+++ src/components/PostLink.tsx
@@ -0,0 +1,20 @@
+import { Post } from "../types/post"
+
+interface PostLinkProps {
+  post: Post
+}
+
+export const PostLink = (props: PostLinkProps) => {
+
+  const { post } = props;
+
+  const url = `http://localhost:5173/posts/${post.id}`
+
+  return (
+    <>
+      <a href={url}>
+        {post.title}
+      </a>
+    </>
+  )
+}
